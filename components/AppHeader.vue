bf89bab94973c5b87be52fb477c2457a0a30ca44
Allen_Yeh <happy1294123@gmail.com>
2025-03-21 17:41:11 +0800

    init

---
+++ components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>this</div>
+    this is header
+</template>
+
+<script lang="ts" setup>
+// const year: string = '2022'
+// const title: string = `${year} iThome 鐵人賽`
+</script>
