c5d466b2f2859f4f208fadfa7323e9a5f2546e51
Allen_Yeh <happy1294123@gmail.com>
2025-03-21 21:53:03 +0800

    build layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div
+        class="bg-[url(/background.jpg)] opacity-50 bg-repeat bg-center absolute bg-cover h-full w-full"
+    ></div>
+    <div class="p-16 opacity-100">
+        <slot />
+    </div>
+</template>
